fff081cee52c8f811315184f4b4f3b494cb2687f
彥志 駱 <crack15951@yahoo.com.tw>
2018-03-29 16:20:55 +0800

    start

---
+++ ASP/Areas/HomeWork1/Controllers/IndexController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.Areas.HomeWork1.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: HomeWork1/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP/Areas/HomeWork1/Models/Orders.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.Areas.HomeWork1.Models
+{
+    public class Orders
+    {
+        [DisplayName("訂單編號")]
+        public int OrderID { get; set; }
+        [DisplayName("客戶編號")]
+        public int CustomerID { get; set; }
+        [DisplayName("員工編號")]
+        public int EmployeeID { get; set; }
+
+        [DisplayName("訂單日期")]
+        public DateTime OrderDate { get; set; }
+        [DisplayName("需要日期")]
+        public DateTime RequiredDate { get; set; }
+        [DisplayName("出貨日期")]
+        public DateTime ShippedDate { get; set; }
+
+        [DisplayName("出貨編號")]
+        public int ShipperID { get; set; }
+        public Decimal Freight { get; set; }
+        [DisplayName("出貨地址")]
+        public Char[] ShipAddress { get; set; }
+        [DisplayName("出貨城市")]
+        public Char[] ShipCity { get; set; }
+        [DisplayName("出貨地區")]
+        public Char[] ShipRegion { get; set; }
+        [DisplayName("郵遞區號")]
+        public Char[] ShipPostalCode { get; set; }
+        [DisplayName("出貨國家")]
+        public Char[] ShipCountry { get; set; }
+
+    }
+}
